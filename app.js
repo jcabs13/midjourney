a1f0ff1a81a5fa8fe9af8c94b169ee146b71b347
jcabs13 <jcabshear@gmail.com>
2023-08-01 16:04:28 -0600

    using envirometn variable

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const Discord = require('discord.js');
 const axios = require('axios');
 const express = require('express');
+require('dotenv').config(); // to load variables from .env file into process.env
+
 const app = express();
 app.use(express.json());
 
 const bot = new Discord.Client();
-const webhookUrl = 'https://discord.com/api/webhooks/1136036444947877980/quaWXKXyaR3xYrQcYiG0ZUJqI4lNMHw_472-KBApIwT1GEbzaIYTeKSNPlzDWzgY54Rj';
-const channelID = 'YOUR_DISCORD_CHANNEL_ID'; // replace with your channel ID
+const webhookUrl = process.env.WEBHOOK_URL; // replace with your webhook URL
+const channelID = process.env.DISCORD_CHANNEL_ID; // replace with your channel ID
 
 bot.on('ready', () => {
   console.log('Bot is ready');
 });
 
 bot.on('message', msg => {
+  console.log('Message received on Discord');
   if (msg.channel.id === channelID && msg.author.bot && msg.attachments.first()) {
+    console.log('Sending image to Glide');
     // Replace 'http://example.com/receiveImage' with your Glide API endpoint
     axios.post('http://example.com/receiveImage', {
       imageUrl: msg.attachments.first().url
@@ -27,7 +31,7 @@ bot.on('message', msg => {
   }
 });
 
-bot.login('YOUR_BOT_TOKEN');
+bot.login(process.env.BOT_TOKEN);
 
 app.post('/sendDiscordMessage', (req, res) => {
   console.log('Received POST from Glide');
@@ -40,6 +44,7 @@ app.post('/sendDiscordMessage', (req, res) => {
     return res.sendStatus(400);
   }
 
+  console.log('Sending message to Discord');
   axios({
     method: 'post',
     url: webhookUrl,
@@ -56,16 +61,16 @@ app.post('/sendDiscordMessage', (req, res) => {
   })
   .catch(error => {
     if (error.response) {
-      console.log('Error sending message:', error.response.status);
+      console.log('Error response from Discord:', error.response.status);
       console.log(error.response.data);
       console.log(error.response.headers);
       res.sendStatus(error.response.status);
     } else if (error.request) {
-      console.log(error.request);
+      console.log('Error request:', error.request);
     } else {
-      console.log('Error', error.message);
+      console.log('Error:', error.message);
     }
-    console.log(error.config);
+    console.log('Error config:', error.config);
     res.sendStatus(500);
   });
 });
